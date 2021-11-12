55d6aad5097d871a8cbb04a46d7cc79cd7575c41
covict <tihomir.covic.1974@gmail.com>
2021-11-12 18:48:01 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,29 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CommonModule } from '@angular/common';
+
+// For components using angular-animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Imports of used components 
+import { 
+  MatButtonModule,
+  MatInputModule,
+  MatCardModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
